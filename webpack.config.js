8b926e5ef8f51ae6bde91cb774dab3bb7adfe149
Lena05k <lena05k@yandex.ru>
2023-05-03 12:40:32 +0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
